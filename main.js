1333c5e92d2d24751495411a4a5b09c1af6983f9
GeeeMartins <128103067+GeeeMartins@users.noreply.github.com>
2023-10-05 09:08:00 -0300

    05/10

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 alert('Seja Bem Vindo');
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom(){ document.querySelector('#som_tecla_pom').play(); 
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+function tocaSomTim(){ document.querySelector('#som_tecla_tm').play(); 
+}
